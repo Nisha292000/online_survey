12e3f5337d808eba07e38edffd9cdcbe53fbf479
Nisha <nt200029@gmail.com>
2023-03-31 23:39:56 +0530

    online_survey

--- Api/api/_helpers/status.config.js
+++ Api/api/_helpers/status.config.js
@@ -1,12 +1,6 @@
 const status = {
     UNEXPECTED_ERROR: 'Something went wrong!',
-    ID_WRONG: "Either Email Id or Password is wrong, please try again.",
-    EMAIL_SEND: "Email send successfully",
-    INVALID_TOKEN: 'token is invalid',
     OK: 'OK',
-    DATA_NOT_AVAILABLE: 'Data not found',
-    LOGIN_SUCCESS: 'Login successfully',
-    EMAIL_NOT_FOUND: "The Email Id that you've entered is not found, please try again.",
 }
 
 module.exports = status
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path:"survey", component:SurveyComponent},
+  { path:"**", component:HomeComponent},
 ];
 
 @NgModule({
